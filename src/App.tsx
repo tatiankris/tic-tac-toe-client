87c43f25245408e250300643b90be0a99d068718
tatiankris <tatiankris@mail.ru>
2023-01-11 04:47:57 +0300

    add room, location, turn with sockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import s from './App.module.css';
 import { Box } from './Box';
 import io from 'socket.io-client';
+import { useLocation } from 'react-router';
 const socket = io('http://localhost:5000');
 
 const combinations = [
@@ -16,6 +16,11 @@ const combinations = [
     [2, 4, 6],
 ];
 
+
+const random = () => {
+    return Array.from(Array(8), () => Math.floor(Math.random() * 36).toString(36)).join('');
+};
+
 function App() {
 
     const [myTurn, setMyTurn] = useState(true);
@@ -26,19 +31,14 @@ function App() {
     const [turnData, setTurnData] = useState<boolean | any>(false);
 
   const [game, setGame] = useState(Array(9).fill(''));
-  const [isX, setIsX] = useState(false);
   const [turnNumber, setTurnNumber] = useState(0);
   const [winner, setWinner] = useState(false);
 
-  // const turn = (index: number) => {
-  //   let g = [...game];
-  //   if (!g[index] && !winner) {
-  //     g[index] = isX ? 'X' : 'O';
-  //     setGame(g);
-  //     setIsX(!isX);
-  //     setTurnNumber(turnNumber + 1);
-  //   }
-  // };
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const paramsRoom = params.get('room');
+    const [room, setRoom] = useState(paramsRoom);
+
 
   const turn = (index: number) => {
       if (!game[index] && !winner && myTurn && hasOpponent) {
@@ -46,20 +46,15 @@ function App() {
       }
   };
 
+  const sendRestart = () => {
+      socket.emit('reqRestart', JSON.stringify({ room }));
+  };
+
   const restart = () => {
     setGame(Array(9).fill(''));
     setWinner(false);
     setTurnNumber(0);
   };
-
-  useEffect(() => {
-    // check for winner for every turn
-    combinations.forEach((c) => {
-      if (game[c[0]] === game[c[1]] && game[c[0]] === game[c[2]] && game[c[0]] !== '') {
-        setWinner(true);
-      }
-    });
-  }, [game]);
 
     useEffect(() => {
         socket.on('playerTurn', (json) => {
@@ -76,24 +71,75 @@ function App() {
         });
     }, []);
 
+  useEffect(() => {
+
+    combinations.forEach((c) => {
+      if (game[c[0]] === game[c[1]] && game[c[0]] === game[c[2]] && game[c[0]] !== '') {
+        setWinner(true);
+      }
+    });
+  }, [game]);
+
+
+
+    useEffect(() => {
+        if (turnData) {
+            const data = JSON.parse(turnData);
+            let g = [...game];
+            if (!g[data.index] && !winner) {
+                g[data.index] = data.value;
+                setGame(g);
+                setTurnNumber(turnNumber + 1);
+                setTurnData(false);
+                setMyTurn(!myTurn);
+                setPlayer(data.value);
+            }
+        }
+    }, [turnData, game, turnNumber, winner, myTurn]);
+
+    useEffect(() => {
+        if (paramsRoom) {
+            //  player 2
+            setXO('O');
+            socket.emit('join', paramsRoom);
+            setRoom(paramsRoom);
+            setMyTurn(false);
+        } else {
+            // player 1
+            const newRoomName = random();
+            socket.emit('create', newRoomName);
+            setRoom(newRoomName);
+            setMyTurn(true);
+        }
+    }, [paramsRoom]);
 
 
   return (
       <div className={s.container}>
-        <p>
-          {winner || turnNumber === 9 ? (
-              <button className={s.reset} onClick={restart}>
-                Restart
-              </button>
+          Room: {room}
+          <button className="btn" onClick={() => setShare(!share)}>
+              Share
+          </button>
+          {share ? (
+              <>
+                  <br />
+                  <br />
+                  Share link: <input type="text" value={`${window.location.href}?room=${room}`} readOnly />
+              </>
           ) : null}
-          {winner ? (
-              <span>We have a winner: {!isX ? 'X' : 'O'}</span>
-          ) : turnNumber === 9 ? (
-              <span>It's a tie!</span>
-          ) : (
-              <br />
-          )}
-        </p>
+          <br />
+          <br />
+          Turn: {myTurn ? 'You' : 'Opponent'}
+          <br />
+          {hasOpponent ? '' : 'Waiting for opponent...'}
+          <p>
+              {winner || turnNumber === 9 ? (
+                  <button className="btn" onClick={sendRestart}>
+                      Restart
+                  </button>
+              ) : null}
+              {winner ? <span>We have a winner: {player}</span> : turnNumber === 9 ? <span>It's a tie!</span> : <br />}
+          </p>
 
         <div className={s.row}>
           <Box index={0} turn={turn} value={game[0]} />
